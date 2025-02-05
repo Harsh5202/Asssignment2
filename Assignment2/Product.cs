0a7a549c5aae5c3420ba0a187d1ed924b04d4fbf
Harsh Chauhan <hchauhan5919@conestogac.on.ca>
2025-02-05 17:54:54 -0500

    Created product.cs file

---
+++ Assignment2/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Assignment2
+{
+    public class Product
+    {
+        // Attributes
+        public int ProdID { get; set; } 
+        public string ProdName { get; set; } 
+        public decimal ItemPrice { get; set; } 
+        public int StockAmount { get; set; } 
+
+        // Constructor
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+            if (prodID < 5 || prodID > 50000)
+                throw new ArgumentOutOfRangeException(nameof(prodID), "Product ID must be between 5 and 50000.");
+            if (itemPrice < 5 || itemPrice > 5000)
+                throw new ArgumentOutOfRangeException(nameof(itemPrice), "Price must be between $5 and $5000.");
+            if (stockAmount < 5 || stockAmount > 500000)
+                throw new ArgumentOutOfRangeException(nameof(stockAmount), "Stock must be between 5 and 500000.");
+
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        // Method to increase stock
+        public void IncreaseStock(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Increase amount cannot be negative.");
+            StockAmount += amount;
+        }
+
+        // Method to decrease stock
+        public void DecreaseStock(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount), "Decrease amount cannot be negative.");
+            if (StockAmount - amount < 0)
+                throw new InvalidOperationException("Stock cannot be less than zero.");
+            StockAmount -= amount;
+        }
+    }
+}
--- Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
+++ Assignment2/obj/Debug/net8.0/Assignment2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4aa0fef8feb3e289ed1af1e2a4cfe3ffef9869")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
